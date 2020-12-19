ecd90b9259b0e2a6d9f9ae8a1d10d98ef8a2569b
templain <grandmacks@gmail.com>
2020-12-19 16:10:38 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+import Head from 'next/head'
+
+const Page: FC = () => (
+    <div>
+        <Head>
+            <title>Home</title>
+        </Head>
+        <div>Hello World</div>
+    </div>
+)
+
+export default Page
